1a6456fc5e5dd8ab11088ed63d7ca20dd7761c8c
Motive <motivewc@users.noreply.github.com>
2024-08-24 22:10:57 -0400

    try out goreleaser

--- internal/platform/platform_other.go
+++ internal/platform/platform_other.go
@@ -2,6 +2,10 @@
 
 package platform
 
+import (
+	"fmt"
+)
+
 func RemoveCodesigningSignature(path string) error {
 	fmt.Println("Codesigning is a null op on your OS, TBD if this is OK.")
 	return nil
